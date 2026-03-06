2be9f05da139c01b5597c4cb36d678e5fda3fd9d
Simon Schweizer <simonschweizer@MacBook-Air-von-Simon.local>
2026-03-06 17:52:35 +0100

    Projekt wiederhergestellt + .gitignore

---
+++ cubeTests/cubeTests.swift
@@ -0,0 +1,17 @@
+//
+//  cubeTests.swift
+//  cubeTests
+//
+//  Created by Simon Schweizer on 28.07.2025.
+//
+
+import Testing
+@testable import cube
+
+struct cubeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
